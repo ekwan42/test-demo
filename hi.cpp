8feb23c2c2281b591e114b75df6d55328ad97491
Ekhwan Ahmed <ekhwanahmed42@gmail.com>
2025-01-11 00:33:48 +0600

    tdt

---
+++ hi.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define py cout<<"YES"<<endl;
+#define pn cout<<"NO"<<endl;
+#define endl '\n'
+#define int long long
+#define vi vector<int>
+#define vs vector<string>
+#define pb push_back
+#define whl int t;cin>>t;while(t--)
+int32_t main()
+{
+optimize();
+cout<<"hello"<<endl;
+return 0;
+}
